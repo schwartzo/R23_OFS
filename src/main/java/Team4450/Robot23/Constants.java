c0a79fb71340165e616137725921c1185f9bae4d
rac@racc.com <rac@racc.com>
2023-02-20 12:17:43 -0800

    Switch which end of robot is the "front". It is now the non-scoring end. Allows for robot to start with scoring elements facing driver station wall.

--- src/main/java/Team4450/Robot23/Constants.java
+++ src/main/java/Team4450/Robot23/Constants.java
@@ -24,7 +24,7 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public final class Constants
 {
-	public static String		PROGRAM_NAME = "ORF23-02.18.23-1";
+	public static String		PROGRAM_NAME = "ORF23-02.20.23-1";
 
 	public static Robot			robot;
 
@@ -103,25 +103,25 @@ public final class Constants
 
     // Swerve Module motor controller & encoder port assignments and steering offsets.
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; // Set front left module drive motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; //  Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 3; //  Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(301.113); //  Measure and set front left steer offset
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 10; //  Set left front drive motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11; //  Set left front steer motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 12; //  Set left front steer encoder ID
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(336.621); //  Measure and set left front steer offset
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4; //  Set front right drive motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5; //  Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 6; //  Set front right steer encoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(238.096); //  Measure and set front right steer offset
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 7; //  Set front right drive motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 8; //  Set front right steer motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 9; //  Set front right steer encoder ID
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(235.107); //  Measure and set front right steer offset
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7; //  Set back left drive motor ID
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8; //  Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; //  Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(55.283); //  Measure and set back left steer offset
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; //  Set back left drive motor ID
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 5; //  Set back left steer motor ID
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 6; //  Set back left steer encoder ID
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(58.535); //  Measure and set back left steer offset
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10; //  Set back right drive motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 11; //  Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12; //  Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(157.764); //  Measure and set back right steer offset
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 1; // Set back right module drive motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 2; //  Set back right module steer motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3; //  Set back right steer encoder ID
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(121.201); //  Measure and set back right steer offset
     
 	// Use these values in PathWeaver for speed and acceleration.
     // Robot will go faster than this, more like 3 mps but this value tones down autonomous speed.
